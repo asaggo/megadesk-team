a6ed93390962d174357f96fa1e21ce87153b8efa
larcade-tess <35175520+larcade-tess@users.noreply.github.com>
2018-02-07 15:49:06 -0700

    Json + 2d array

---
+++ megadesk-team-master/MegaDesk-2-JessieJi-TessLarcade/ViewAllQuotes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MegaDesk_4_JessieJi
+{
+    public partial class ViewAllQuotes : Form
+    {
+        public ViewAllQuotes()
+        {
+            InitializeComponent();
+            string[] items = System.IO.File.ReadAllLines("quotes.json");
+            foreach (string item in items)
+            {
+                string[] logLine = item.Split(',');
+                listView1.Items.Add(item);
+            }
+        }
+
+        private void btnBackToMenu_Click(object sender, EventArgs e)
+        {
+            MainMenu mainMenu = (MainMenu)this.Tag;
+            mainMenu.Show();
+            Close();
+        }
+    }
+}
